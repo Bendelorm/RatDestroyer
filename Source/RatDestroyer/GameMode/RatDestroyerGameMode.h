e2622ea40cceba911d8d2ea7a6a27c54d25fd3da
Bendelorm <269416@stud.inn.no>
2024-10-02 16:54:58 +0200

    GameMode and PlayerPawn

---
+++ Source/RatDestroyer/GameMode/RatDestroyerGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RatDestroyerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RATDESTROYER_API ARatDestroyerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
