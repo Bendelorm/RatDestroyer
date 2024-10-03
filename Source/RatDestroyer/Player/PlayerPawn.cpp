036fa3aaa60b343a0b27189a433220362f99159a
Bendelorm <269416@stud.inn.no>
2024-10-03 13:25:49 +0200

    Not fully working camera control

--- Source/RatDestroyer/Player/PlayerPawn.cpp
+++ Source/RatDestroyer/Player/PlayerPawn.cpp
@@ -3,19 +3,39 @@
 
 #include "PlayerPawn.h"
 
+#include "BaseGizmos/GizmoElementShared.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
 // Sets default values
 APlayerPawn::APlayerPawn()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	RootComponent = SceneComponent;
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->TargetArmLength = 2000.0f;
+	SpringArmComponent->bDoCollisionTest = false;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
 // Called when the game starts or when spawned
 void APlayerPawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	//set initial zoom, location, rotation
+	TargetLocation = GetActorLocation();
+	TargetZoom = 3000.f;
+	const FRotator Rotation = SpringArmComponent->GetRelativeRotation();
+	TargetRotation = FRotator(Rotation.Pitch + -50, Rotation.Yaw, 0.0f);
 }
 
 // Called every frame
@@ -23,6 +43,19 @@ void APlayerPawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	CameraBounds();
+
+	//moving the pawn in the correct direction
+	const FVector InterpolatedLocation = UKismetMathLibrary::VInterpTo(GetActorLocation(), TargetLocation, DeltaTime, MoveSpeed);
+	SetActorLocation(InterpolatedLocation);
+
+	//zooming the camera in the correct direction
+	const float InterpolatedZoom = UKismetMathLibrary::FInterpTo(SpringArmComponent->TargetArmLength, TargetZoom, DeltaTime, ZoomSpeed);
+	SpringArmComponent->TargetArmLength = InterpolatedZoom;
+
+	//rotate the camera in the correct direction
+	const FRotator InterpolatedRotation = UKismetMathLibrary::RInterpTo(SpringArmComponent->GetRelativeRotation(), TargetRotation, DeltaTime, RotateSpeed);
+	SpringArmComponent->SetRelativeRotation(InterpolatedRotation);
 }
 
 // Called to bind functionality to input
@@ -30,5 +63,106 @@ void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &APlayerPawn::Forward);
+	PlayerInputComponent->BindAxis(TEXT("Right"), this, &APlayerPawn::Right);
+	PlayerInputComponent->BindAxis(TEXT("Zoom"), this, &APlayerPawn::Zoom);
+	PlayerInputComponent->BindAxis(TEXT("RotateHorizontal"), this, &APlayerPawn::RotateHorizontal);
+	PlayerInputComponent->BindAxis(TEXT("RotateVertical"), this, &APlayerPawn::RotateVertical);
+
+	PlayerInputComponent->BindAction(TEXT("RotateRight"), IE_Pressed, this, &APlayerPawn::RotateRight);
+	PlayerInputComponent->BindAction(TEXT("RotateLeft"), IE_Pressed, this, &APlayerPawn::RotateLeft);
+	PlayerInputComponent->BindAction(TEXT("Rotate"), IE_Pressed, this, &APlayerPawn::EnableRotate);
+	PlayerInputComponent->BindAction(TEXT("Rotate"), IE_Released, this, &APlayerPawn::DisableRotate);
+
 }
 
+void APlayerPawn::Forward(float AxisValue)
+{
+	if(AxisValue == 0.f)
+	{
+		return;
+	}
+	TargetLocation = SpringArmComponent->GetForwardVector() * AxisValue * MoveSpeed * TargetLocation;
+}
+
+void APlayerPawn::Right(float AxisValue)
+{
+	if (AxisValue == 0.f)
+	{
+		return;
+	}
+	TargetLocation = SpringArmComponent->GetRightVector() * AxisValue * MoveSpeed * TargetLocation;
+}
+
+void APlayerPawn::Zoom(float AxisValue)
+{
+	if (AxisValue == 0.f)
+	{
+		return;
+	}
+	const float Zoom = AxisValue * 100.0f;
+	TargetZoom = FMath::Clamp(Zoom + TargetZoom, MinZoom, MaxZoom);
+}
+
+void APlayerPawn::RotateRight()
+{
+	TargetRotation = UKismetMathLibrary::ComposeRotators(TargetRotation, FRotator(0.0f, -45, 0.0f));
+}
+
+void APlayerPawn::RotateLeft()
+{
+	TargetRotation = UKismetMathLibrary::ComposeRotators(TargetRotation, FRotator(0.0f, 45, 0.0f));
+}
+
+void APlayerPawn::EnableRotate()
+{
+	CanRotate = true;
+}
+
+void APlayerPawn::DisableRotate()
+{
+	CanRotate = false;
+}
+
+void APlayerPawn::RotateHorizontal(float AxisValue)
+{
+	if (AxisValue == 0.f)
+	{
+		return;
+	}
+	if (CanRotate)
+	{
+		TargetRotation = UKismetMathLibrary::ComposeRotators(TargetRotation, FRotator(0.0f, AxisValue, 0.f));
+	}
+}
+
+void APlayerPawn::RotateVertical(float AxisValue)
+{
+	if (AxisValue == 0.f)
+	{
+		return;
+	}
+	if (CanRotate)
+	{
+		TargetRotation = UKismetMathLibrary::ComposeRotators(TargetRotation, FRotator(AxisValue, 0.f, 0.f));
+	}
+}
+
+void APlayerPawn::CameraBounds()
+{
+	float NewPitch = TargetRotation.Pitch;
+	if(TargetRotation.Pitch < (RotatePitchMax * -1))
+	{
+		NewPitch = (RotatePitchMax * -1);
+	}
+	else if(TargetRotation.Pitch > (RotatePitchMin * -1))
+	{
+		NewPitch = (RotatePitchMin * -1);
+	}
+
+	//Set the new pitch and clamp any roll
+	TargetRotation = FRotator(NewPitch, TargetRotation.Yaw, 0.f);
+
+	//Clamp desired location to within bounds
+	TargetLocation = FVector(TargetLocation.X, TargetLocation.Y, 0.f);
+}
--- Source/RatDestroyer/Player/PlayerPawn.h
+++ Source/RatDestroyer/Player/PlayerPawn.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Pawn.h"
 #include "PlayerPawn.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class RATDESTROYER_API APlayerPawn : public APawn
 {
@@ -15,15 +18,92 @@ public:
 	// Sets default values for this pawn's properties
 	APlayerPawn();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+	//Components for player
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Player")
+	USceneComponent* SceneComponent;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Player")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Player")
+	UCameraComponent* CameraComponent;
+
+
+	//Functions for camera control
+	UFUNCTION()
+	void Forward(float AxisValue);
+
+	UFUNCTION()
+	void Right(float AxisValue);
+
+	UFUNCTION()
+	void Zoom(float AxisValue);
+
+	UFUNCTION()
+	void RotateRight();
+
+	UFUNCTION()
+	void RotateLeft();
+
+	UFUNCTION()
+	void EnableRotate();
+
+	UFUNCTION()
+	void DisableRotate();
+
+	UFUNCTION()
+	void RotateHorizontal(float AxisValue);
+
+	UFUNCTION()
+	void RotateVertical(float AxisValue);
+
+	UFUNCTION()
+	void CameraBounds();
+
+
+	//Player controlled camera settings
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CameraSettings")
+	float MoveSpeed = 20.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CameraSettings")
+	float RotateSpeed = 2.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CameraSettings")
+	float RotatePitchMin = 10.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CameraSettings")
+	float RotatePitchMax = 80.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CameraSettings")
+	float ZoomSpeed = 2.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CameraSettings")
+	float MinZoom = 500.0f;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "CameraSettings")
+	float MaxZoom = 4000.0f;
+
+	UPROPERTY()
+	FVector TargetLocation;
+
+	UPROPERTY()
+	FRotator TargetRotation;
+
+	UPROPERTY()
+	float TargetZoom;
+
+	UPROPERTY()
+	bool CanRotate;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 };
