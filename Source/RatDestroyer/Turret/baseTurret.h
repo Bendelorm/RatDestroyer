52fb151fcb1e33e923d589f6c572438da2c8615d
mgranum1 <266515@stud.inn.no>
2024-10-16 20:18:54 +0200

    minor update

--- Source/RatDestroyer/Turret/baseTurret.h
+++ Source/RatDestroyer/Turret/baseTurret.h
@@ -32,7 +32,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Variable")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variable")
 	int32 baseDmg;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variable")
