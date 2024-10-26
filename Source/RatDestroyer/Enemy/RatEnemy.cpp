ffca89a26821d5adb4e718322e7eb984d82acac0
ErikEnoksen <269411@stud.inn.no>
2024-10-26 19:01:43 +0200

    Adjusted RatEnemy and added WaveManager

--- Source/RatDestroyer/Enemy/RatEnemy.cpp
+++ Source/RatDestroyer/Enemy/RatEnemy.cpp
@@ -1,64 +1,64 @@
-//// Fill out your copyright notice in the Description page of Project Settings.
-//
-//
-//#include "RatEnemy.h"
-//#include "GameFramework/CharacterMovementComponent.h"
-////#include "RatDestroyer/TreeTesting/RDTreeNode.h"
-//
-//// Sets default values
-//ARatEnemy::ARatEnemy()
-//{
-// 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-//	PrimaryActorTick.bCanEverTick = true;
-//
-//	MovementSpeed = 300.0f;
-//	Health = 10;
-//	bIsMoving = false;
-//	Damage = 1;
-//	
-//	GetCharacterMovement()->MaxWalkSpeed = MovementSpeed;
-//}
-//
-//// Called when the game starts or when spawned
-//void ARatEnemy::BeginPlay()
-//{
-//	Super::BeginPlay();
-//	
-//}
-//
-//
-//// Called every frame put in kode for pathfinding for enemy
-//// void ARatEnemy::Tick(float DeltaTime)
-//// {
-//// 	Super::Tick(DeltaTime);
-////
-////
-//// 	if (bIsMoving && ChildNode() > 0 && CurrentNode < ARDTreeNode())
-//// 	{
-//// 		FVector TargetLocation = ARDTreeNode()[CurrentNode]->GetActorLocation();
-//// 		FVector Direction =(GetTargetLocation()- GetActorLocation().GetSafeNormal());
-//// 		FVector NewLocation = GetActorLocation() + Direction * MovementSpeed * DeltaTime;
-////
-//// 		SetActorLocation(NewLocation);
-////
-//// 		if (FVector::Dist(GetActorLocation(), TargetLocation) < 10.0f)
-//// 		{
-//// 			CurrentNode++;
-////
-//// 			if (CurrentNode >= ARDTreeNode())
-//// 			{
-//// 				bIsMoving = false;
-//// 			}
-//// 		}
-//// 	}
-//// }
-//// void ARatEnemy::Path
-//// {
-//// 	ARDTreeNode = newPath;
-////}
-//// void ARatEnemy::StartMoving()
-//// {
-//// 	bIsMoving = true;
-//// }
-//
-//
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RatEnemy.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+
+ARatEnemy::ARatEnemy()
+{
+ 	// Set this character to call Tick() every frame. 
+	PrimaryActorTick.bCanEverTick = true;
+	MovementSpeed = 300.0f;
+	Health = 10;
+	bIsMoving = false;
+	
+	GetCharacterMovement()->MaxWalkSpeed = MovementSpeed;
+}
+
+// Called when the game starts or when spawned
+void ARatEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+// Called every frame put in code for pathfinding for enemy
+void ARatEnemy::Tick(float DeltaTime)
+ {
+	Super::Tick(DeltaTime);
+
+	if(isAlive)
+	{
+		MoveToNextNode();
+	}
+
+ }
+
+//Function to reduce RatEnemy Health  by the damage number and check if Rat Enemy is still alive
+bool ARatEnemy::AttackEnemy(float DamageTaken)
+{
+	this->Health -= DamageTaken;
+	
+	if(this->Health <= 0)
+	{
+		isAlive = false;
+	}
+	//Returns the status of enemy (true if its Alive and False if not)
+	return isAlive;
+}
+
+void ARatEnemy::SpawnEnemy()
+{
+	this->isAlive = true;
+	//Må add inn her om pathfinding
+}
+
+void ARatEnemy::MoveToNextNode()
+{
+	
+}
+
+
