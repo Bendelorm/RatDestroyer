84ca3eadec7c36ccfc48c87e9ad7494d73876767
mgranum1 <266515@stud.inn.no>
2024-10-27 14:22:12 +0100

    Commented out CPP Node

--- Source/RatDestroyer/Map/Node.cpp
+++ Source/RatDestroyer/Map/Node.cpp
@@ -1,12 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Map/Node.h"
-
-Node::Node()
-{
-}
-
-Node::~Node()
-{
-}
+//#include "Map/Node.h"
+//
+//Node::Node()
+//{
+//}
+//
+//Node::~Node()
+//{
+//}
